bb1eed0f7274472f08cb10b10b6aaadd19b31433
Rafael <rafaelsoares.p.f@gmail.com>
2023-04-11 23:53:14 -0300

    tarefa1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from "react";
+import "./App.css";
 
 function App() {
+  const [nota1, setNota1] = useState(0);
+  const [nota2, setNota2] = useState(0);
+  const [resultado, setResultado] = useState(0);
+  const [opcao, setOpcao] = useState(0);
+
+  const calcular = () => {
+    if (opcao == "Somar")
+      return parseFloat(nota1) + parseFloat(nota2)
+    else if (opcao == "Subtrair")
+      return parseFloat(nota1) - parseFloat(nota2)
+    else if (opcao == "Multiplicar")
+      return parseFloat(nota1) * parseFloat(nota2)
+    else if (opcao == "Dividir")
+      return parseFloat(nota1) / parseFloat(nota2)
+    else //media
+      return parseFloat(nota1) + parseFloat(nota2)/2
+  }
+
+  useEffect(() => {
+    setResultado(calcular());
+  }, [nota1,nota2,opcao]);
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Digite a Nota1 e Nota2</h1>
+      <label>Nota1: </label>
+      <input type="number" value={nota1} onChange={(e) => setNota1(e.target.value)}/>
+      <label>Nota2: </label>
+      <input type="number" value={nota2} onChange={(e) => setNota2(e.target.value)}/>
+
+      <select onChange= {(e) => setOpcao(e.target.value)}>
+        <option>Media</option>
+        <option>Somar</option>
+        <option>Subtrair</option>
+        <option>Multiplicar</option>
+        <option>Dividir</option>
+        
+      </select>
+
+      <label>Resultado = {resultado}</label>
     </div>
   );
 }
